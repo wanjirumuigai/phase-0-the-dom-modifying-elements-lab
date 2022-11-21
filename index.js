f8e78c6115f170842d20fadd8be3e0f6da3cb7f0
Pauline Muigai <wanjirumuigai@gmail.com>
2022-11-21 21:46:24 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+const del = document.getElementById("main");
+del.remove();
+
+const newHeader = document.createElement("h1");
+newHeader.setAttribute('id','victory');
+newHeader.innerHTML="<h1>YOUR-NAME is the champion</h1>";
